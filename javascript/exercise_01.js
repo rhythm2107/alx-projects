4d851d7223660f7cafbbed10e175d3bed2ae842b
rhythm2107 <kamil.krzywda.21@gmail.com>
2024-06-25 11:25:45 +0200

    Add solutions to exercise_01 and exercise_02

---
+++ javascript/exercise_01.js
@@ -0,0 +1,11 @@
+var zm1 = "ciekawe";
+var zm2 = "programowanie";
+var zm3 = "jest";
+var zm4 = "wciagajace";
+var zm5 = "i";
+var pomoc;
+
+
+
+console.log(zm1, zm2, zm3, zm4, zm5);
+//programowanie jest ciekawe i wciagajace\
